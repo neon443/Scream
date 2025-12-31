85daea17702f716d61704223c978e7e75a3993a6
neon443 <69979447+neon443@users.noreply.github.com>
2025-12-31 19:35:12 +0000

    Initial Commit

---
+++ ScreamTests/ScreamTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScreamTests.swift
+//  ScreamTests
+//
+//  Created by neon443 on 31/12/2025.
+//
+
+import Testing
+@testable import Scream
+
+struct ScreamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
